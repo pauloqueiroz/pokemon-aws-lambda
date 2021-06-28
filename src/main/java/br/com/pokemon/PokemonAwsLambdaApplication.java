adb5fa87e84d9a3bf89e0b727910865ed49f4043
Paulo Sérgio <pauloqueiroz16@gmail.com>
2021-06-28 00:06:53 -0300

    Adicionadas configurações para execução da api via lambda function

--- src/main/java/br/com/pokemon/PokemonAwsLambdaApplication.java
+++ src/main/java/br/com/pokemon/PokemonAwsLambdaApplication.java
@@ -2,9 +2,10 @@ package br.com.pokemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PokemonAwsLambdaApplication {
+public class PokemonAwsLambdaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokemonAwsLambdaApplication.class, args);
